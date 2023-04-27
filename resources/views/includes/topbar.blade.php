adeee244902e5f685440952b603b2a3d2f788357
sreerajsree <sreeandsreeraj@gmail.com>
2023-04-27 15:48:34 +0530

    logout changes

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -6,10 +6,8 @@
                 <svg class="icon icon-lg">
                     <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
                 </svg>
-            </button><a class="header-brand d-md-none" href="#">
-                <svg width="118" height="46" alt="CoreUI Logo">
-                    <use xlink:href="/assets/brand/coreui.svg#full"></use>
-                </svg></a>
+            </button><a class="header-brand d-md-none" href="/home">
+                <img class="logo" src="/assets/img/apsensys.png" alt=""></a>
             <ul class="header-nav d-none d-md-flex">
                 <div class="input-group"><span class="input-group-text">
                         <svg class="icon">
@@ -17,11 +15,33 @@
                         </svg></span>
                     <input placeholder="Search" id="name" type="text" class="form-control" name="name"
                         required autocomplete="name">
-                        
+
                 </div>
-                
+                <div class="btn-group">
+                    <button type="button" class="btn dropdown-toggle" data-coreui-toggle="dropdown"
+                        aria-expanded="false" style="margin-left:10px; color:white;"><span><svg class="icon">
+                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-power-standby"></use>
+                        </svg></span>
+                        Logout
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();"><span><svg class="icon">
+                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                        </svg></span>&nbsp;&nbsp;Logout</a>
+                        </li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </ul>
+                </div>
             </ul>
-            <ul class="header-nav ms-auto text-white"><div id="cTime" class="p-2"></div><div class="p-2">|</div><div id="Day" class="p-2"></div></ul>
+            <ul class="header-nav ms-auto text-white">
+                <div id="cTime" class="p-2"></div>
+                <div class="p-2">|</div>
+                <div id="Day" class="p-2"></div>
+            </ul>
             <ul class="header-nav ms-3">
                 <li class="nav-item"><a class="nav-link" href="{{ route('announcements') }}">
                         <svg class="icon icon-lg">
@@ -32,7 +52,8 @@
             <ul class="header-nav ms-3">
                 <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#"
                         role="button" aria-haspopup="true" aria-expanded="false">
-                        <div class="avatar"><img class="avatar-md" style="border-radius: 50%; object-fit: cover;" src="/ProfileImages/{{ Auth::user()->profile_pic }}">
+                        <div class="avatar"><img class="avatar-md" style="border-radius: 50%; object-fit: cover;"
+                                src="/ProfileImages/{{ Auth::user()->profile_pic }}">
                             <span class="avatar-status bg-success"></span>
                         </div>
                     </a>
@@ -45,21 +66,12 @@
                             <svg class="icon me-2">
                                 <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
                             </svg> Profile</a>
-                            {{-- <a class="dropdown-item" href="#">
+                        {{-- <a class="dropdown-item" href="#">
                             <svg class="icon me-2">
                                 <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
                             </svg> Settings
                             </a> --}}
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                                <svg class="icon me-2">
-                                    <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
-                                </svg> Logout</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+                        <div class="dropdown-divider"></div>
                     </div>
                 </li>
             </ul>
